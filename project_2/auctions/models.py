cc9adb5d06584c6d07a54b8abec7ee137f03d4da
pepsp <josec1000_0@hotmail.com>
2024-04-13 17:02:33 -0700

    Created models

--- project_2/auctions/models.py
+++ project_2/auctions/models.py
@@ -4,3 +4,37 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=300)
+    image_url = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
+    watchlist = models.ManyToManyField(User, blank=True, null=True, related_name="listing_watchlist")
+    price = models.ForeignKey('Bid',blank=True, null=True, on_delete=models.CASCADE, related_name="listing_price")
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.category
+
+class Bid(models.Model):
+    bid = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="bids")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, blank=True, null=True, related_name="bids")
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="author_comment")
+    comment = models.CharField(max_length=300)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, null=True, blank=True, related_name="listing_comment")
+
+    def __str__(self):
+        return f"{self.author} comment on {self.listing}"
